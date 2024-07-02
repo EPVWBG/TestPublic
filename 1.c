2020230658aa51afbc25c75afdf849e7b98383f6
wenbin.ge <wenbin.ge@epvsystem.com>
2024-07-02 10:12:03 +0800

    modify for 1.c

--- 1.c
+++ 1.c
@@ -3,9 +3,7 @@
 int main()
 {
     printf("This is the first branch!\n");
-<<<<<<< HEAD
     printf("First modify on the branch master!\n");
-=======
->>>>>>> iss01
+    printf("add this line to the remote!\n");
     return 0;
 }
